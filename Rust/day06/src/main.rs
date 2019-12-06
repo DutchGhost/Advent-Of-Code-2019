cf7f3f646bfe919749946f45d8868b3a9fb7a77f
Dodo <kasper199914@gmail.com>
2019-12-06 17:34:16 +0100

    part 2

--- Rust/day06/src/main.rs
+++ Rust/day06/src/main.rs
@@ -1,9 +1,6 @@
 static PUZZLE: &'static str = include_str!(r"..\..\..\Inputs\day06.txt");
 
-mod orbit;
-use orbit::*;
-
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 fn parse(s: &str) -> (&str, &str) {
     let mut splitter = s.split(")");
@@ -11,10 +8,9 @@ fn parse(s: &str) -> (&str, &str) {
     (splitter.next().unwrap(), splitter.next().unwrap())
 }
 
-fn recursive<'a>(orbittee: &'a str, map: &OrbitGraph<&'a str, HashSet<&'a str>>) -> usize {
-    let mut count = 0;
+fn recursive<'a>(orbittee: &'a str, map: &HashMap<&'a str, HashSet<&'a str>>) -> usize {
 
-    map.orbit_index(&orbittee)
+    map.get(&orbittee)
         .map(|orbits| {
             orbits
                 .iter()
@@ -23,23 +19,58 @@ fn recursive<'a>(orbittee: &'a str, map: &OrbitGraph<&'a str, HashSet<&'a str>>)
         .unwrap_or(0)
 }
 
-fn part1(s: &str) -> usize {
-    let mut orbits: OrbitGraph<&str, HashSet<&str>> = OrbitGraph::new();
-
-    for line in PUZZLE.lines() {
-        let (orbittee, orbitter) = parse(line);
-        orbits.orbit(orbittee).add(orbitter);
-        orbits.orbit(orbitter);
-    }
-
+fn part1<'a>(orbits: &HashMap<&'a str, HashSet<&'a str>>) -> usize {
     orbits
         .iter()
         .map(|(orbittee, _)| recursive(orbittee, &orbits))
         .sum()
 }
 
-fn main() {
-    let p1 = part1(PUZZLE);
+fn recfind<'a>(
+    stack: &mut Vec<&'a str>,
+    orbittee: &'a str,
+    map: &HashMap<&'a str, HashSet<&'a str>>,
+) {
+    map.iter()
+        .find(|(_, v)| v.contains(orbittee))
+        .map(|(k, _)| {
+            stack.push(k);
 
-    println!("Part 1: {}\n", p1);
+            recfind(stack, k, map);
+        });
+}
+
+fn part2<'a>(orbits: &HashMap<&'a str, HashSet<&'a str>>) -> usize {
+    let mut san = Vec::new();
+
+    recfind(&mut san, "SAN", &orbits);
+
+    let mut you = Vec::new();
+
+    recfind(&mut you, "YOU", &orbits);
+
+    you.iter()
+        .enumerate()
+        .filter_map(|(meidx, me)| {
+            san.iter()
+                .position(|santa| santa == me)
+                .map(|santaidx| santaidx + meidx)
+        })
+        .min()
+        .unwrap()
+}
+
+fn main() {
+    let mut orbits: HashMap<&str, HashSet<&str>> = HashMap::new();
+
+    for line in PUZZLE.lines() {
+        let (orbittee, orbitter) = parse(line);
+        orbits.entry(orbittee).or_insert_with(HashSet::default).insert(orbitter);
+        orbits.entry(orbitter).or_insert_with(HashSet::default);
+    }
+
+    let p1 = part1(&orbits);
+    let p2 = part2(&orbits);
+
+    println!("Part 1: {}\nPart 2: {}", p1, p2);
 }
