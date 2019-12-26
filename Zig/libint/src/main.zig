57a04d88420287031050d453d0f70c42504516a9
Dodo <kasper199914@gmail.com>
2019-12-26 21:30:25 +0100

    minor changes. Run tests at comptime

--- Zig/libint/src/main.zig
+++ Zig/libint/src/main.zig
@@ -101,6 +101,7 @@ pub fn Machine(comptime IO: type) type {
         const Self = @This();
         const Polled = Poll(void);
 
+        /// Creates a new machine from `memory` and `io`.
         pub fn init(memory: []isize, io: IO) Self {
             return Self{
                 .memory = memory,
@@ -108,14 +109,14 @@ pub fn Machine(comptime IO: type) type {
             };
         }
 
-        pub fn write(self: *Self, addr: usize, value: isize) !void {
+        fn write(self: *Self, addr: usize, value: isize) !void {
             if (addr >= self.memory.len) {
                 return error.OutOfBounds;
             }
             self.memory[addr] = value;
         }
 
-        pub fn write_operand(self: *Self, index: usize, value: isize, mode: Mode) !void {
+        fn write_operand(self: *Self, index: usize, value: isize, mode: Mode) !void {
             var offset = self.esp + index;
 
             var addr = switch (mode) {
@@ -127,14 +128,14 @@ pub fn Machine(comptime IO: type) type {
             try self.write(@intCast(usize, addr), value);
         }
 
-        pub fn read(self: *const Self, addr: usize) !isize {
+        fn read(self: *const Self, addr: usize) !isize {
             if (addr >= self.memory.len) {
                 return error.OutOfBounds;
             }
             return self.memory[addr];
         }
 
-        pub fn read_operand(self: *const Self, index: usize, mode: Mode) !isize {
+        fn read_operand(self: *const Self, index: usize, mode: Mode) !isize {
             var offset = self.esp + index;
             var addr = switch (mode) {
                 Mode.Immediate => @intCast(isize, index),
@@ -145,7 +146,7 @@ pub fn Machine(comptime IO: type) type {
             return self.read(@intCast(usize, addr));
         }
 
-        pub fn add(self: *Self, modes: []const Mode) !Polled {
+        fn add(self: *Self, modes: []const Mode) !Polled {
             var r1 = try self.read_operand(1, modes[0]);
             var r2 = try self.read_operand(2, modes[1]);
 
@@ -155,7 +156,7 @@ pub fn Machine(comptime IO: type) type {
             return Polled.Pending;
         }
 
-        pub fn mul(self: *Self, modes: []const Mode) !Polled {
+        fn mul(self: *Self, modes: []const Mode) !Polled {
             var r1 = try self.read_operand(1, modes[0]);
             var r2 = try self.read_operand(2, modes[1]);
 
@@ -165,7 +166,7 @@ pub fn Machine(comptime IO: type) type {
             return Polled.Pending;
         }
 
-        pub fn save(self: *Self, modes: []const Mode) !Polled {
+        fn save(self: *Self, modes: []const Mode) !Polled {
             var value = switch (self.io.read()) {
                 PollVariant.Pending => return Polled.Pending,
                 PollVariant.Ready => |item| try item,
@@ -183,7 +184,7 @@ pub fn Machine(comptime IO: type) type {
             return Polled.Pending;
         }
 
-        pub fn output(self: *Self, modes: []const Mode) !Polled {
+        fn output(self: *Self, modes: []const Mode) !Polled {
             var value = try self.read_operand(1, modes[0]);
             switch (self.io.write(value)) {
                 PollVariant.Pending => return Polled.Pending,
@@ -194,7 +195,7 @@ pub fn Machine(comptime IO: type) type {
             return Polled.Pending;
         }
 
-        pub fn jump_if_true(self: *Self, modes: []const Mode) !Polled {
+        fn jump_if_true(self: *Self, modes: []const Mode) !Polled {
             var cond = try self.read_operand(1, modes[0]);
             var esp = try self.read_operand(2, modes[1]);
 
@@ -207,7 +208,7 @@ pub fn Machine(comptime IO: type) type {
             return Polled.Pending;
         }
 
-        pub fn jump_if_false(self: *Self, modes: []const Mode) !Polled {
+        fn jump_if_false(self: *Self, modes: []const Mode) !Polled {
             var cond = try self.read_operand(1, modes[0]);
             var esp = try self.read_operand(2, modes[1]);
 
@@ -220,7 +221,7 @@ pub fn Machine(comptime IO: type) type {
             return Polled.Pending;
         }
 
-        pub fn less_than(self: *Self, modes: []const Mode) !Polled {
+        fn less_than(self: *Self, modes: []const Mode) !Polled {
             var r1 = try self.read_operand(1, modes[0]);
             var r2 = try self.read_operand(2, modes[1]);
 
@@ -232,7 +233,7 @@ pub fn Machine(comptime IO: type) type {
             return Polled.Pending;
         }
 
-        pub fn equals(self: *Self, modes: []const Mode) !Polled {
+        fn equals(self: *Self, modes: []const Mode) !Polled {
             var r1 = try self.read_operand(1, modes[0]);
             var r2 = try self.read_operand(2, modes[1]);
 
@@ -253,26 +254,42 @@ pub fn Machine(comptime IO: type) type {
             return Polled.Pending;
         }
 
-        pub fn halt(self: *Self) MachineError!Polled {
+        fn halt(self: *Self) MachineError!Polled {
             return Polled{ .Ready = {} };
         }
 
+        /// Attempts to run one step of the intcode program.
+        /// Returns `Pending` when the program has not finished,
+        /// `Ready` when the program has finished, or an error
+        /// whenever something went wrong.
         pub fn poll(self: *Self) !Polled {
             var opcode = try Opcode.from(try self.read(self.esp));
-            var modes = opcode.modes;
+            var modes = opcode.modes[0..];
 
             return switch (opcode.mnemonic) {
-                Mnemonic.Add => self.add(modes[0..]),
-                Mnemonic.Mul => self.mul(modes[0..]),
-                Mnemonic.Save => self.save(modes[0..]),
-                Mnemonic.Output => self.output(modes[0..]),
-                Mnemonic.JumpIfTrue => self.jump_if_true(modes[0..]),
-                Mnemonic.JumpIfFalse => self.jump_if_false(modes[0..]),
-                Mnemonic.LessThan => self.less_than(modes[0..]),
-                Mnemonic.Equals => self.equals(modes[0..]),
-                Mnemonic.AdjustBase => self.adjust_base(modes[0..]),
+                Mnemonic.Add => self.add(modes),
+                Mnemonic.Mul => self.mul(modes),
+                Mnemonic.Save => self.save(modes),
+                Mnemonic.Output => self.output(modes),
+                Mnemonic.JumpIfTrue => self.jump_if_true(modes),
+                Mnemonic.JumpIfFalse => self.jump_if_false(modes),
+                Mnemonic.LessThan => self.less_than(modes),
+                Mnemonic.Equals => self.equals(modes),
+                Mnemonic.AdjustBase => self.adjust_base(modes),
                 Mnemonic.Halt => self.halt(),
             };
+        }
+
+        /// Runs the whole intcode program. Returns an
+        /// error whenever something went wrong, a
+        /// void otherwise.
+        pub fn run(self: *Self) !void {
+            while (true) {
+                switch (try self.poll()) {
+                    PollVariant.Pending => continue,
+                    PollVariant.Ready => return,
+                }
+            }
         }
     };
 }
@@ -314,29 +331,25 @@ test "" {
         }
     };
 
-    var slice = [_]isize{ 1, 1, 1, 4, 99, 5, 6, 0, 99 };
+    comptime {
+        var slice = [_]isize{ 1, 1, 1, 4, 99, 5, 6, 0, 99 };
 
-    const modes = [_]Mode{Mode.Position};
+        var m = Machine(Dummy).init(slice[0..], Dummy.init());
 
-    var m = Machine(Dummy).init(slice[0..], Dummy.init());
+        try m.run();
 
-    while (true) {
-        var r = try m.poll();
-
-        switch (r) {
-            PollVariant.Pending => continue,
-            PollVariant.Ready => break,
-        }
+        @import("std").debug.assert(slice[0] == 30);
     }
-    @import("std").debug.assert(slice[0] == 30);
 }
 
 test "opcode" {
-    var opcode = try Opcode.from(21003);
+    comptime {
+        var opcode = try Opcode.from(21003);
 
-    const testing = @import("std").testing;
+        const testing = @import("std").testing;
 
-    testing.expectEqual(opcode.modes[0], Mode.Position);
-    testing.expectEqual(opcode.modes[1], Mode.Immediate);
-    testing.expectEqual(opcode.modes[2], Mode.Relative);
+        testing.expectEqual(opcode.modes[0], Mode.Position);
+        testing.expectEqual(opcode.modes[1], Mode.Immediate);
+        testing.expectEqual(opcode.modes[2], Mode.Relative);
+    }
 }
