c504c56477a0928fb9110308ff01aaef0df998d5
Dodo <kasper199914@gmail.com>
2020-02-17 19:58:05 +0100

    use HRTB's

--- Rust/day06/src/get.rs
+++ Rust/day06/src/get.rs
@@ -2,20 +2,36 @@ use std::borrow::Borrow;
 use std::collections::HashMap;
 use std::hash::Hash;
 
+/// Given a reference to `K`,
+/// finds the corresponding element
+/// in a container.
 pub trait Get<K: ?Sized> {
     type Output;
 
-    fn get(&self, key: &K) -> Option<Self::Output>;
+    fn get(&self, key: &K) -> Option<&Self::Output>;
 }
 
-impl<'a, K, V: 'a, Q> Get<Q> for &'a HashMap<K, V>
+impl<K: ?Sized, T> Get<K> for &'_ T
+where
+    T: Get<K>,
+{
+    type Output = T::Output;
+
+    #[inline(always)]
+    fn get(&self, key: &K) -> Option<&Self::Output> {
+        (&**self).get(key)
+    }
+}
+
+impl<K, V, Q: ?Sized> Get<Q> for HashMap<K, V>
 where
     K: Borrow<Q> + Hash + Eq,
     Q: Hash + Eq,
 {
-    type Output = &'a V;
+    type Output = V;
 
-    fn get(&self, key: &Q) -> Option<Self::Output> {
+    #[inline(always)]
+    fn get(&self, key: &Q) -> Option<&Self::Output> {
         HashMap::get(self, &key)
     }
 }
--- Rust/day06/src/main.rs
+++ Rust/day06/src/main.rs
@@ -11,35 +11,24 @@ fn parse(s: &str) -> (&str, &str) {
     (splitter.next().unwrap(), splitter.next().unwrap())
 }
 
-fn orbitters_of<'a, K: 'a, S>(orbittee: K, map: &S) -> usize
+fn orbitters_of<K: ?Sized, S>(orbittee: &K, map: &S) -> usize
 where
     S: Get<K>,
-    S::Output: IntoIterator<Item = &'a K>,
-    K: Copy,
+    for<'out> &'out S::Output: IntoIterator<Item = &'out K>,
 {
     Get::get(map, &orbittee)
         .map(|orbits| {
-            orbits.into_iter().fold(0, |count, orbitter| {
-                count + 1 + orbitters_of(*orbitter, map)
-            })
+            orbits
+                .into_iter()
+                .fold(0, |count, orbitter| count + 1 + orbitters_of(orbitter, map))
         })
         .unwrap_or(0)
 }
 
-// fn orbitters_of<'a>(orbittee: &'a str, map: &HashMap<&'a str, HashSet<&'a str>>) -> usize {
-//     map.get(&orbittee)
-//         .map(|orbits| {
-//             orbits
-//                 .iter()
-//                 .fold(0, |count, orbitter| count + 1 + orbitters_of(orbitter, map))
-//         })
-//         .unwrap_or(0)
-// }
-
 fn part1<'a>(orbits: &HashMap<&'a str, HashSet<&'a str>>) -> usize {
     orbits
         .iter()
-        .map(|(orbittee, _): (&&str, _)| orbitters_of::<&str, _>(orbittee, &orbits))
+        .map(|(orbittee, _): (&&str, _)| orbitters_of(orbittee, &orbits))
         .sum()
 }
 
